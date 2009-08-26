b4bf343991ef6a56a964d8632f436a94ed870bc4
Sam Aaron <samaaron@gmail.com>
2009-08-26 20:02:56 +0200

    add UDPServerWithCount

---
+++ lib/osc/udp_server_with_count.rb
@@ -0,0 +1,18 @@
+module OSC
+  class UDPServerWithCount < UDPServer
+    attr_reader :num_messages_received
+
+    def initialize
+      @num_messages_received = 0
+      super
+    end
+
+    def serve
+      loop do
+        p, sender = recvfrom(MAX_MSG_SIZE)
+        dispatch p
+        @num_messages_received += 1
+      end
+    end
+  end
+end
